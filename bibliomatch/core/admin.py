fc5755701b535a654ce5eb5b5505c16a379ce6a3
Luis Toscano <luismariotoscanopalomino@gmail.com>
2025-05-22 19:45:23 -0500

    Eliminacion de proyecto antiguo

--- bibliomatch/core/admin.py
+++
@@ -1,6 +0,0 @@
-#from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
-#from django.contrib import admin
-
-#admin.site.register(SocialApp)
-#admin.site.register(SocialAccount)
-#admin.site.register(SocialToken)
--- bibliomatch/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-@login_required
-def inicio(request):
-    return render(request, 'inicio.html')
